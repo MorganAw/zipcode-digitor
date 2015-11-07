d7facd0da63dfb4f586f6880db1df2917a59bf52
Wentao Xu <contactwentaoxu@gmail.com>
2015-11-07 13:40:29 -0800

    Fixed run

--- src/shared/components/home_body.jsx
+++ src/shared/components/home_body.jsx
@@ -9,19 +9,28 @@ export class HomeBody extends React.Component {
   render() {
     console.log('*** Rendering home_body ***');
     return (
-      <head>
-      <Link to={'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'}</Link>
-      <Link to={'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'}</Link>
-      </head>
-      <body>
       <div>
+      <h2>Test</h2>
+      // <nav class="navbar navbar-default navbar-static-top" role="navigation">
+      //   <div class="container">
+      //     <!-- Brand and toggle get grouped for better mobile display -->
+      //     <div class="navbar-header">
+      //       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      //         <span class="sr-only">Toggle navigation</span>
+      //         <span class="icon-bar"></span>
+      //         <span class="icon-bar"></span>
+      //         <span class="icon-bar"></span>
+      //       </button>
+      //       <a class="navbar-brand" href="#">Accurent</a>
+      //     </div>
+      //   </div>
+      // </nav>
         <h2>Welcome to MCAws Simple Node Webapp</h2>
         <p>Looks like stuff is running, time to start experimenting</p>
 
         <h1>H O M E P A G E B O Y S</h1>
         <Link to={`/tenant`}>Login</Link>
       </div>
-      </body>
     );
   }
 };
