f9e672cc20384a5b93e0138030699b0d7a8cc919
Wentao Xu <contactwentaoxu@gmail.com>
2015-11-07 12:44:28 -0800

    comments fixed

--- src/shared/components/home_body.jsx
+++ src/shared/components/home_body.jsx
@@ -8,7 +8,7 @@ export class home_body extends React.Component {
     console.log('*** Rendering home_body ***');
     return (
       <div>
-        <h2>Welcome to MCAw's Simple Node Webapp</h2>
+        <h2>Welcome to MCAws Simple Node Webapp</h2>
         <p>Looks like stuff is running, time to start experimenting</p>
       </div>
     );
