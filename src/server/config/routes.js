1f16b3b5d1a8b31b215061aae4e47fd3ed9802cb
Morgan <morgancaw@gmail.com>
2015-11-08 03:06:59 -0800

    [Morgan] ... I guess I modified routes somehow? ... ... yaa

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -34,9 +34,9 @@ export function express_router(app, router) {
   });
 
   // Heroku postgres connection
-  router.get('/db', function (req, res) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM test_table', function(err, result) {
+  router.get('/db', (req, res) => {
+    pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+      client.query('SELECT * FROM test_table', (err, result) => {
         done();
         if (err)
          { console.error(err); res.send("Error " + err); }
